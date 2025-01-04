49c67bca462dd929791023d2801a1f6550ef3b43
linnlathtun <linnlathtun99.mm@gmail.com>
2025-01-04 19:01:11 +0700

    read profiles from json and add to db

--- src/main/java/per/llt/spring_tinder_ai/SpringAiTinderApplication.java
+++ src/main/java/per/llt/spring_tinder_ai/SpringAiTinderApplication.java
@@ -1,19 +1,13 @@
 package per.llt.spring_tinder_ai;
 
-import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import per.llt.spring_tinder_ai.conversations.ChatMessages;
-import per.llt.spring_tinder_ai.conversations.Conversation;
 import per.llt.spring_tinder_ai.conversations.ConversationRepository;
-import per.llt.spring_tinder_ai.profiles.Profile;
-import per.llt.spring_tinder_ai.profiles.Gender;
+import per.llt.spring_tinder_ai.profiles.ProfileCreationService;
 import per.llt.spring_tinder_ai.profiles.ProfileRepository;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootApplication
 public class SpringAiTinderApplication implements CommandLineRunner {
@@ -25,7 +19,8 @@ public class SpringAiTinderApplication implements CommandLineRunner {
     private ConversationRepository conversationRepository;
 
     @Autowired
-    private OpenAiChatClient openAiChatClient;
+    private ProfileCreationService profileCreationService;
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiTinderApplication.class, args);
@@ -33,46 +28,12 @@ public class SpringAiTinderApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        //I want to be clear all conversations because of using AI when creating a branch of profiles.
-        profileRepository.deleteAll();
+        clearAllData();
+        profileCreationService.saveProfilesToDB();
+    }
+
+    private void clearAllData() {
         conversationRepository.deleteAll();
-
-        Profile profile = new Profile(
-                "1",
-                "lin",
-                "lat",
-                25,
-                "Myanmar",
-                Gender.FEMALE,
-                "Software programmer",
-                "foo.jpg",
-                "INTP"
-        );
-
-        Profile profile1 = new Profile(
-                "2",
-                "Tun",
-                "Myat",
-                35,
-                "Myanmar",
-                Gender.FEMALE,
-                "Software programmer",
-                "foo.jpg",
-                "INTP"
-        );
-
-        profileRepository.save(profile);
-        profileRepository.save(profile1);
-        profileRepository.findAll().forEach(System.out::println);
-
-
-        Conversation conversation = new Conversation(
-                "1",
-                profile.id(),
-                List.of(new ChatMessages("Hello", profile.id(), LocalDateTime.now()))
-        );
-
-        conversationRepository.save(conversation);
-        conversationRepository.findAll().forEach(System.out::println);
+        profileRepository.deleteAll();
     }
 }
--- src/main/java/per/llt/spring_tinder_ai/profiles/ProfileRepository.java
+++ src/main/java/per/llt/spring_tinder_ai/profiles/ProfileRepository.java
@@ -1,9 +1,16 @@
 package per.llt.spring_tinder_ai.profiles;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfileRepository extends MongoRepository<Profile, String> {
 
+    /**
+     * 🚀  an aggregation stage in MongoDB that retrieves random documents.
+     */
+
+    @Aggregation(pipeline = {"{ $sample: { size: 1 } }"})
+    Profile getRandomProfile();
 }
